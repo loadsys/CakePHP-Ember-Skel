f74749290fb4e11a51f5f924b22aa05220d626b3
Joey Trapp <joey@loadsys.com>
2012-11-30 00:00:30 -0600

    Removed wrap from build while it is being tested

--- grunt.js
+++ grunt.js
@@ -97,5 +97,5 @@ module.exports = function(grunt) {
   grunt.registerTask('default', 'concat');
 
   // Build task.
-  grunt.registerTask('build', 'concat min wrap');
+  grunt.registerTask('build', 'concat min');
 };
